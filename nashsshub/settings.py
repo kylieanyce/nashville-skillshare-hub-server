d58d4b8687f58bda6d09044a5a34f323b25e06f4
kylieanyce <kylieanyce@gmail.com>
2021-06-22 13:52:15 -0500

    added fixtures and changed settings toa llow heroku

--- nashsshub/settings.py
+++ nashsshub/settings.py
@@ -10,10 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import django_on_heroku
+import os
 from pathlib import Path
 import environ
 env = environ.Env()
 environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
